26e99e5ebe98df3e9344c6a423e81e4f5b8e2bfc
vien <136911935+kvus@users.noreply.github.com>
2026-03-17 15:47:59 +0700

    Fix static file path for production environment

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV !== "production")
 app.use("/api/tasks", taskRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
